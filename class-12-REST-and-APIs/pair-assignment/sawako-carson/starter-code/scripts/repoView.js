2ba02be6c9ee1748eeffde28758638015a7ba114
Carson Daniel Parker <Carson@dkparker.com>
2016-01-19 14:46:21 -0800

    Sawako and Carson pair assignment 12

---
+++ class-12-REST-and-APIs/pair-assignment/sawako-carson/starter-code/scripts/repoView.js
@@ -0,0 +1,33 @@
+(function(module) {
+  var repoView = {};
+
+  // DONE: Private methods declared here live only within the scope of the wrapping IIFE.
+  var ui = function() {
+    var $about = $('#about'); // Best practice: Cache the DOM query if it's used more than once.
+
+    $about.find('ul').empty();
+    $about.show().siblings().hide();
+  };
+
+  // DONE: How do you want to render a single repo as html? Return your filled in HTML template.
+  var render = function(repo) {
+    var repoList = document.createElement('li');
+    $(repoList).append('<div>'+repo.full_name+'</div><div>'+repo.html_url+'</div><div>'+repo.description+'</div>');
+    console.log(repoList);
+    return repoList;
+  };
+
+  // DONE: If all the data is loaded, we can prep the UI and render the repos.
+  repoView.index = function() {
+    ui();
+
+    // The jQuery `append` method lets us append an entire array of HTML elements at once,
+    // So we can use a little FP to transform our data-set into DOM nodes:
+    $('#about ul').append(
+      repos.with('full_name').map(render)
+
+    );
+  };
+
+  module.repoView = repoView;
+})(window);
